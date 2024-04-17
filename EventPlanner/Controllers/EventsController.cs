dea38b1b232dcdeceeea8f9ed33e8f2968e384f0
Chris Klanderman <klanderman.chris@gmail.com>
2024-04-17 00:45:29 -0400

    Created events controller. Added a delete endpoint

---
+++ EventPlanner/Controllers/EventsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventPlanner.Database;
+using EventPlanner.Models;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    private readonly EventPlannerDbContext _dbContext;
+
+    public EventsController(EventPlannerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    // DELETE: api/events/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteEvent(int id)
+
+    {
+        var @event = await _dbContext.Events.FindAsync(id);
+        if (@event == null)
+        {
+            return NotFound();
+        }
+
+        _dbContext.Events.Remove(@event);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
